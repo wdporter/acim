2da4cd4f6bcb6dd04c596e2510b5e67ed8a4c6a4
David Porter <david.porter@fastmail.com>
2021-06-15 19:51:10 +1000

    put div inside details

--- .eleventy.js
+++ .eleventy.js
@@ -9,35 +9,6 @@ module.exports = cfg => {
 		return new CleanCSS({}).minify(code).styles;
 	});
 
-	let markdownIt = require("markdown-it")({html: true});
-	let markdownItContainer = require("markdown-it-container");
-  /*let options = {
-    html: true
-  };*/
-	let markdownLib = markdownIt.use(markdownItContainer, "details",
-		{
-			validate: function (params) {
-				return params.trim().match(/^details\s+(.*)$/);
-			},
-
-			render: function (tokens, idx) {
-				var m = tokens[idx].info.trim().match(/^details\s+(.*)$/);
-
-				if (tokens[idx].nesting === 1) {
-					// opening tag
-					return '<details><summary>' + markdownIt.utils.escapeHtml(m[1]) + '</summary>\n';
-
-				} else {
-					// closing tag
-					return '</details>\n';
-				}
-			}
-		}
-
-	);
-
-	cfg.setLibrary("md", markdownLib);
-
 	return {
 		dir: {
 			input: "src"
