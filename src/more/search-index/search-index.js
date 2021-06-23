9cbd1b3266df73c6c285df2628f2c895dde8caea
David Porter <david.porter@fastmail.com>
2021-06-23 21:12:50 +1000

    unknown

--- src/more/search-index/search-index.js
+++ src/more/search-index/search-index.js
@@ -412,11 +412,58 @@ export default data = [
 	{
 		"id": "T-1.VI.5",
 		"url": "/text/1-the-meaning-of-miracles/vi-the-illusion-of-needs/#p5",
-		"body": "All aspects of fear are untrue because they do not exist at the creative level, and therefore do not exist at all. To whatever extent you are willing to submit your beliefs to this test, to that extent are your perceptions corrected. In sorting out the false from the true, the miracle proceeds along these lines: Perfect love casts out fear. If fear exists, Then there is not perfect love. But: Only perfect love exists. If there is fear, It produces a state that does not exist. Believe this and you will be free. Only God can establish this solution, and this faith <em>is</em> His gift."
+		"body": "All aspects of fear are untrue because they do not exist at the creative level, and therefore do not exist at all. To whatever extent you are willing to submit your beliefs to this test, to that extent are your perceptions corrected. In sorting out the false from the true, the miracle proceeds along these lines: Perfect love casts out fear. If fear exists, Then there is not perfect love. But: Only perfect love exists. If there is fear, It produces a state that does not exist. Believe this and you will be free. Only God can establish this solution, and this faith is His gift."
 	},
-
-
-
+	{
+		"id": "T-1.VII.1",
+		"url": "/text/1-the-meaning-of-miracles/vii-distortions-of-miracle-impulses/#p1",
+		"body": "Your distorted perceptions produce a dense cover over miracle impulses, making it hard for them to reach your own awareness. The confusion of miracle impulses with physical impulses is a major perceptual distortion. Physical impulses are misdirected miracle impulses. All real pleasure comes from doing God’s Will. This is because not doing it is a denial of Self. Denial of Self results in illusions, while correction of the error brings release from it. Do not deceive yourself into believing that you can relate in peace to God or to your brothers with anything external."
+	},
+	{
+		"id": "T-1.VII.2",
+		"url": "/text/1-the-meaning-of-miracles/vii-distortions-of-miracle-impulses/#p2",
+		"body": "Child of God, you were created to create the good, the beautiful and the holy. Do not forget this. The Love of God, for a little while, must still be expressed through one body to another, because vision is still so dim. You can use your body best to help you enlarge your perception so you can achieve real vision, of which the physical eye is incapable. Learning to do this is the body’s only true usefulness."
+	},
+	{
+		"id": "T-1.VII.3",
+		"url": "/text/1-the-meaning-of-miracles/vii-distortions-of-miracle-impulses/#p3",
+		"body": "Fantasy is a distorted form of vision. Fantasies of any kind are distortions, because they always involve twisting perception into unreality. Actions that stem from distortions are literally the reactions of those who know not what they do. Fantasy is an attempt to control reality according to false needs. Twist reality in any way and you are perceiving destructively. Fantasies are a means of making false associations and attempting to obtain pleasure from them. But although you can perceive false associations, you can never make them real except to yourself. You believe in what you make. If you offer miracles, you will be equally strong in your belief in them. The strength of your conviction will then sustain the belief of the miracle receiver. Fantasies become totally unnecessary as the wholly satisfying nature of reality becomes apparent to both giver and receiver. Reality is lost through usurpation, which produces tyranny. As long as a single slave remains to walk the earth, your release is not complete. Complete restoration of the Sonship is the only goal of the miracle-minded."
+	},
+	{
+		"id": "T-1.VII.4",
+		"url": "/text/1-the-meaning-of-miracles/vii-distortions-of-miracle-impulses/#p4",
+		"body": "This is a course in mind training. All learning involves attention and study at some level. Some of the later parts of the course rest too heavily on these earlier sections not to require their careful study. You will also need them for preparation. Without this, you may become much too fearful of what is to come to make constructive use of it. However, as you study these earlier sections, you will begin to see some of the implications that will be amplified later on."
+	},
+	{
+		"id": "T-1.VII.5",
+		"url": "/text/1-the-meaning-of-miracles/vii-distortions-of-miracle-impulses/#p5",
+		"body": "A solid foundation is necessary because of the confusion between fear and awe to which I have already referred, and which is often made. I have said that awe is inappropriate in connection with the Sons of God, because you should not experience awe in the presence of your equals. However, it was also emphasized that awe is proper in the Presence of your Creator. I have been careful to clarify my role in the Atonement without either overstating or understating it. I am also trying to do the same with yours. I have stressed that awe is not an appropriate reaction to me because of our inherent equality. Some of the later steps in this course, however, involve a more direct approach to God Himself. It would be unwise to start on these steps without careful preparation, or awe will be confused with fear, and the experience will be more traumatic than beatific. Healing is of God in the end. The means are being carefully explained to you. Revelation may occasionally reveal the end to you, but to reach it the means are needed."
+	},
+	{
+		"id": "T-2.I.1",
+		"url": "/text/2-the-separation-and-the-atonement/i-the-origins-of-separation/#p1",
+		"body": "To extend is a fundamental aspect of God which He gave to His Son. In the creation, God extended Himself to His creations and imbued them with the same loving Will to create. You have not only been fully created, but have also been created perfect. There is no emptiness in you. Because of your likeness to your Creator you are creative. No child of God can lose this ability because it is inherent in what he is, but he can use it inappropriately by projecting. The inappropriate use of extension, or projection, occurs when you believe that some emptiness or lack exists in you, and that you can fill it with your own ideas instead of truth. This process involves the following steps: First, you believe that what God created can be changed by your own mind. Second, you believe that what is perfect can be rendered imperfect or lacking. Third, you believe that you can distort the creations of God, including yourself. Fourth, you believe that you can create yourself, and that the direction of your own creation is up to you."
+	},
+	{
+		"id": "T-2.I.2",
+		"url": "/text/2-the-separation-and-the-atonement/i-the-origins-of-separation/#p2",
+		"body": "These related distortions represent a picture of what actually occurred in the separation, or the detour into fear. None of this existed before the separation, nor does it actually exist now. Everything God created is like Him. Extension, as undertaken by God, is similar to the inner radiance that the children of the Father inherit from Him. Its real source is internal. This is as true of the Son as of the Father. In this sense the creation includes both the creation of the Son by God, and the Son’s creations when his mind is healed. This requires God’s endowment of the Son with free will, because all loving creation is freely given in one continuous line, in which all aspects are of the same order."
+	},
+	{
+		"id": "T-2.I.3",
+		"url": "/text/2-the-separation-and-the-atonement/i-the-origins-of-separation/#p3",
+		"body": "The Garden of Eden, or the preseparation condition, was a state of mind in which nothing was needed. When Adam listened to the lies of the serpent, all he heard was untruth. You do not have to continue to believe what is not true unless you choose to do so. All that can literally disappear in the twinkling of an eye because it is merely a misperception. What is seen in dreams seems to be very real. Yet the Bible says that a deep sleep fell upon Adam, and nowhere is there reference to his waking up. The world has not yet experienced any comprehensive reawakening or rebirth. Such a rebirth is impossible as long as you continue to project or miscreate. It still remains within you, however, to extend as God extended His Spirit to you. In reality this is your only choice, because your free will was given you for your joy in creating the perfect."
+	},
+	{
+		"id": "T-2.I.4",
+		"url": "/text/2-the-separation-and-the-atonement/i-the-origins-of-separation/#p4",
+		"body": "All fear is ultimately reducible to the basic misperception that you have the ability to usurp the power of God. Of course, you neither can nor have been able to do this. Here is the real basis for your escape from fear. The escape is brought about by your acceptance of the Atonement, which enables you to realize that your errors never really occurred. Only after the deep sleep fell upon Adam could he experience nightmares. If a light is suddenly turned on while someone is dreaming a fearful dream, he may initially interpret the light itself as part of his dream and be afraid of it. However, when he awakens, the light is correctly perceived as the release from the dream, which is then no longer accorded reality. This release does not depend on illusions. The knowledge that illuminates not only sets you free, but also shows you clearly that you are free."
+	},
+	{
+		"id": "T-2.I.5",
+		"url": "/text/2-the-separation-and-the-atonement/i-the-origins-of-separation/#p5",
+		"body": "Whatever lies you may believe are of no concern to the miracle, which can heal any of them with equal ease. It makes no distinctions among misperceptions. Its sole concern is to distinguish between truth on the one hand, and error on the other. Some miracles may seem to be of greater magnitude than others. But remember the first principle in this course; there is no order of difficulty in miracles. In reality you are perfectly unaffected by all expressions of lack of love. These can be from yourself and others, from yourself to others, or from others to you. Peace is an attribute in you. You cannot find it outside. Illness is some form of external searching. Health is inner peace. It enables you to remain unshaken by lack of love from without and capable, through your acceptance of miracles, of correcting the conditions proceeding from lack of love in others."
+	},
 
 
 
